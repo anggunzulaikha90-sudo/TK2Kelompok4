226618c45ecd74cf430ce8d144fb0ef49aa5b600
auliaasmarani <auliaasmarani456@gmail.com>
2026-09-21 20:59:30 +0700

    perubahan 9

--- src/Makanan/Main.java
+++ src/Makanan/Main.java
@@ -25,8 +25,10 @@ public class Main {
         System.out.println("Menu Tiga\n"+"Nama Makanan\t: " + m3.getNamaMakanan()+ "\nHarga\t\t: " + m3.getHarga() + "\nKategori\t: " + m3.getKategori());
         cireng m4 = new cireng("Cireng", 1000, "Makanan Ringan");
         System.out.println("Menu Empat\n"+"Nama Makanan\t: " + m4.getNamaMakanan()+ "\nHarga\t\t: " + m4.getHarga() + "\nKategori\t: " + m4.getKategori());
-        batagor m5 = new batagor("Batagor\n", 5000, "Makanan Ringan");
+        batagor m5 = new batagor("Batagor", 5000, "Makanan Ringan");
         System.out.println("Menu Lima\n"+"Nama Makanan\t: " + m5.getNamaMakanan()+ "\nHarga\t\t: " + m5.getHarga() + "\nKategori\t: " + m5.getKategori());
+        jenangSumSum m6 = new jenangSumSum("jenangSumSum", 6000, "Makanan Ringan");
+        System.out.println("Menu Enam\n"+"Nama Makanan\t: " + m6.getNamaMakanan()+ "\nHarga\t\t: " + m6.getHarga() + "\nKategori\t: " + m6.getKategori());
     }
     
 }
---
+++ src/Makanan/jenangSumSum.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Makanan;
+
+/**
+ *
+ * @author USERR
+ */
+public class jenangSumSum {
+    String namaMakanan;
+    int harga;
+    String kategori;
+
+    public String getNamaMakanan() {
+        return namaMakanan;
+    }
+
+    public void setNamaMakanan(String namaMakanan) {
+        this.namaMakanan = namaMakanan;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public jenangSumSum(String namaMakanan, int harga, String kategori) {
+        this.namaMakanan = namaMakanan;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+    
+}
